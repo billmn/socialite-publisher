650b442776f2786326ead60711a69fafb26dfd14
Davide Bellini <davide.bellini@selfcomposer.com>
2015-01-23 17:36:35 +0100

    Drivers improvements

--- app/Social/Driver.php
+++ app/Social/Driver.php
@@ -2,6 +2,7 @@
 
 use GuzzleHttp\Client;
 use Laravel\Socialite\Contracts\Factory as Socialite;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 
 abstract class Driver {
 
@@ -19,11 +20,16 @@ abstract class Driver {
 			"services.{$this->driver}.redirect" => route('social.callback', $this->driver)
 		]);
 
-		$this->client    = new Client;
-		$this->provider  = $socialite->driver($this->getDriver());
+		$this->client    = $this->getClient();
+		$this->provider  = $socialite->driver($this->driver);
 		$this->socialite = $socialite;
 	}
 
+	/**
+	 * Get driver name
+	 *
+	 * @return string
+	 */
 	public function getDriver()
 	{
 		$class = class_basename(get_class($this));
@@ -31,51 +37,116 @@ abstract class Driver {
 		return strtolower($class);
 	}
 
+	/**
+	 * Check current driver
+	 *
+	 * @param  string  $driver Driver name
+	 * @return boolean
+	 */
 	public function is($driver)
 	{
 		return strtolower($driver) === $this->driver;
 	}
 
+	/**
+	 * Social Authentication
+	 *
+	 * @return \Symfony\Component\HttpFoundation\RedirectResponse
+	 */
 	public function authenticate()
 	{
 		return $this->provider->redirect();
 	}
 
+	/**
+	 * Authenticated User
+	 *
+	 * @return array
+	 */
 	public function user()
 	{
 		return (array) $this->provider->user();
 	}
 
+	/**
+	 * Get Http Client
+	 *
+	 * @return \GuzzleHttp\Client
+	 */
 	public function getClient()
 	{
-		return $this->client;
+		return new Client;
 	}
 
+	/**
+	 * Instance of current provider
+	 *
+	 * @return mixed
+	 */
 	public function getProvider()
 	{
 		return $this->provider;
 	}
 
+	/**
+	 * Instance of Socialite
+	 *
+	 * @return \Laravel\Socialite\Contracts\Factory
+	 */
 	public function getSocialite()
 	{
 		return $this->socialite;
 	}
 
-	public function getToken()
+	/**
+	 * Get Consumer Key
+	 *
+	 * @return string
+	 */
+	public function getKey()
 	{
-		return $this->getInfo('token');
+		return config("services.{$this->driver}.client_id");
 	}
 
+	/**
+	 * Get Consumer Secret
+	 *
+	 * @return string
+	 */
+	public function getSecret()
+	{
+		return config("services.{$this->driver}.client_secret");
+	}
+
+	/**
+	 * Get Social information (user fields, token, ...)
+	 *
+	 * @param  null|string $key
+	 * @return mixed
+	 */
 	public function getInfo($key = null)
 	{
-		$file    = app('filesystem.disk');
-		$content = $file->get("{$this->driver}.json");
+		$file     = app('filesystem.disk');
+		$fileName = "{$this->driver}.json";
 
-		$info = json_decode($content, true);
+		if ($file->exists($fileName))
+		{
+			$content = $file->get($fileName);
 
-		return $key ? array_get($info, $key) : $info;
+			$info = json_decode($content, true);
+
+			return $key ? array_get($info, $key) : $info;
+		}
+
+		return [];
 	}
 
+	/**
+	 * Save Social informations
+	 *
+	 * @param  array  $info
+	 * @return array|false
+	 */
 	public function saveInfo(array $info)
 	{
 		$file    = app('filesystem.disk');
@@ -89,7 +160,12 @@ abstract class Driver {
 		return false;
 	}
 
-
+	/**
+	 * Publish to a Social Network
+	 *
+	 * @param  array  $values
+	 * @return mixed
+	 */
 	abstract public function publish(array $values);
 
 }
