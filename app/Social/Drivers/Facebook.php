8a8f9bb011527de9d373d778b85b114c803ed1a9
Davide Bellini <davide.bellini@selfcomposer.com>
2015-01-23 17:36:20 +0100

    Docblocks on Facebook

--- app/Social/Drivers/Facebook.php
+++ app/Social/Drivers/Facebook.php
@@ -8,7 +8,18 @@ use Laravel\Socialite\Contracts\Factory as Socialite;
 
 class Facebook extends Driver {
 
-	private $url     = "https://graph.facebook.com";
+	/**
+	 * The base Facebook Graph URL.
+	 *
+	 * @var string
+	 */
+	private $url = "https://graph.facebook.com";
+
+	/**
+	 * The Graph API version for the request.
+	 *
+	 * @var string
+	 */
 	private $version = "v2.2";
 
 	public function __construct(Socialite $socialite)
@@ -18,6 +29,9 @@ class Facebook extends Driver {
 		$this->provider->scopes(config('services.facebook.scopes'));
 	}
 
+	/**
+	 * {@inheritdoc}
+	 */
 	public function saveInfo(array $info)
 	{
 		$facebookInfo = $info + [
@@ -28,54 +42,9 @@ class Facebook extends Driver {
 		return parent::saveInfo($facebookInfo);
 	}
 
-	public function getPages()
-	{
-		try
-		{
-			$token    = $this->getInfo('token');
-			$response = $this->client->get("{$this->url}/{$this->version}/me/accounts?access_token={$token}");
-
-			$body  = $response->json();
-			$pages = $body['data'];
-
-			$pages = array_sort($pages, function($value)
-			{
-				return $value['name'];
-			});
-
-			return $pages;
-		}
-		catch (RequestException $e)
-		{
-			throw $e;
-		}
-	}
-
-	public function getPage($pageId)
-	{
-		try
-		{
-			$token    = $this->getInfo('token');
-			$response = $this->client->get("{$this->url}/{$this->version}/{$pageId}?access_token={$token}");
-
-			return $response->json();
-		}
-		catch (RequestException $e)
-		{
-			throw $e;
-		}
-	}
-
-	public function getPageToken($pageId)
-	{
-		$page = array_where($this->getPages(), function($key, $value) use($pageId)
-		{
-			return $value['id'] == $pageId;
-		});
-
-		return array_get(head($page), 'access_token', false);
-	}
-
+	/**
+	 * {@inheritdoc}
+	 */
 	public function publish(array $values)
 	{
 		try
@@ -105,6 +74,71 @@ class Facebook extends Driver {
 
 			throw new ClientException($response['error']['message']);
 		}
+	}
+
+	/**
+	 * Get user's pages
+	 *
+	 * @return array
+	 */
+	public function getPages()
+	{
+		try
+		{
+			$token    = $this->getInfo('token');
+			$response = $this->client->get("{$this->url}/{$this->version}/me/accounts?access_token={$token}");
+
+			$body  = $response->json();
+			$pages = $body['data'];
+
+			$pages = array_sort($pages, function($value)
+			{
+				return $value['name'];
+			});
+
+			return $pages;
+		}
+		catch (RequestException $e)
+		{
+			throw $e;
+		}
+	}
+
+	/**
+	 * Get user's page informations
+	 *
+	 * @param  string $pageId
+	 * @return array
+	 */
+	public function getPage($pageId)
+	{
+		try
+		{
+			$token    = $this->getInfo('token');
+			$response = $this->client->get("{$this->url}/{$this->version}/{$pageId}?access_token={$token}");
+
+			return $response->json();
+		}
+		catch (RequestException $e)
+		{
+			throw $e;
+		}
+	}
+
+	/**
+	 * Get user's page token
+	 *
+	 * @param  string $pageId
+	 * @return string
+	 */
+	public function getPageToken($pageId)
+	{
+		$page = array_where($this->getPages(), function($key, $value) use($pageId)
+		{
+			return $value['id'] == $pageId;
+		});
+
+		return array_get(head($page), 'access_token', false);
 	}
 
 }
